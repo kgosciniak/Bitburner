b8b3cc44e727097e2b2163897ebd3a282f8a13c2
Kyle Gosciniak <pretzel_boy@comcast.net>
2021-12-23 19:42:43 -0500

    alias scan=home; run scan.js

--- scan.js
+++ scan.js
@@ -6,7 +6,6 @@ let facServers = {
     "The-Cave" : "orange",
     "w0r1d_d43m0n" : "red"
 };
-
 let svObj = (name = 'home', depth = 0) => ({ name: name, depth: depth });
 export function getServers(ns) {
     let result = [];
